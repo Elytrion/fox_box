9d503912e142c27218b91cd0c31e4355b2831924
Jed Goh <electronfirework@gmail.com>
2026-07-20 13:03:24 +0800

    Added logger outputs for test scene

--- src/scenes/testScene.cpp
+++ src/scenes/testScene.cpp
@@ -1,4 +1,5 @@
 #include "sceneManager/sceneManager.h"
+#include "renderer2D/logger.hpp"
 #include <imgui.h>
 
 class TestScene : Scene
@@ -8,7 +9,9 @@ public:
 	{ }
 
 	void init() override
-	{}
+	{
+		LOG_INFO("{} - Initialising scene", __func__);
+	}
 
 	void update() override
 	{
@@ -16,6 +19,8 @@ public:
 	}
 
 	void exit()
-	{}
+	{
+		LOG_INFO("{} - Exiting scene", __func__);
+	}
 };
 TestScene _instance; // create a instance to register scene
